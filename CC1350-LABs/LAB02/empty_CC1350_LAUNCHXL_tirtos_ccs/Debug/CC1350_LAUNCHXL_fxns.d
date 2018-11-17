56d50c2bd23dbd672ab4535ea4adecd85280dda8
villasen <vmartinvilla@gmail.com>
2018-11-16 21:59:13 -0800

    Submitting lab 2 of CC1350
    Submitting lab 2 of CC1350

---
+++ CC1350-LABs/LAB02/empty_CC1350_LAUNCHXL_tirtos_ccs/Debug/CC1350_LAUNCHXL_fxns.d
@@ -0,0 +1,77 @@
+# FIXED
+
+CC1350_LAUNCHXL_fxns.obj: ../CC1350_LAUNCHXL_fxns.c
+CC1350_LAUNCHXL_fxns.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/include/stdbool.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/include/stddef.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/include/stdint.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/ioc.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_types.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/../inc/hw_chip_def.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_memmap.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_ioc.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_ints.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/interrupt.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_nvic.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/debug.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/cpu.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_cpu_scs.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../driverlib/rom.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/gpio.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_gpio.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/drivers/rf/RF.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/drivers/dpl/ClockP.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/include/stddef.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/drivers/dpl/SemaphoreP.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/include/stddef.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/DeviceFamily.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/rf_common_cmd.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/rf_mailbox.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/include/string.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/include/linkage.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/rf_prop_cmd.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/rf_ble_cmd.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/drivers/pin/PINCC26XX.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/include/stddef.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/drivers/dpl/HwiP.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/include/stddef.h
+CC1350_LAUNCHXL_fxns.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/drivers/PIN.h
+CC1350_LAUNCHXL_fxns.obj: ../Board.h
+CC1350_LAUNCHXL_fxns.obj: ../CC1350_LAUNCHXL.h
+
+../CC1350_LAUNCHXL_fxns.c: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/include/stdbool.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/include/stddef.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/include/stdint.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/ioc.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_types.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/../inc/hw_chip_def.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_memmap.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_ioc.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_ints.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/interrupt.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_nvic.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/debug.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/cpu.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_cpu_scs.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../driverlib/rom.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/gpio.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_gpio.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/drivers/rf/RF.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/drivers/dpl/ClockP.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/include/stddef.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/drivers/dpl/SemaphoreP.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/include/stddef.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/DeviceFamily.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/rf_common_cmd.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/rf_mailbox.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/include/string.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/include/linkage.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/rf_prop_cmd.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/rf_ble_cmd.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/drivers/pin/PINCC26XX.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/include/stddef.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/drivers/dpl/HwiP.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/include/stddef.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/drivers/PIN.h: 
+../Board.h: 
+../CC1350_LAUNCHXL.h: 
---
+++ CC1350-LABs/LAB02/empty_CC1350_LAUNCHXL_tirtos_ccs/Debug/subdir_rules.mk
@@ -0,0 +1,43 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Each subdirectory must supply rules for building sources it contributes
+CC1350_LAUNCHXL.obj: ../CC1350_LAUNCHXL.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: ARM Compiler'
+	"C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/bin/armcl" -mv7M3 --code_state=16 --float_support=vfplib -me --include_path="C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source" --include_path="C:/Users/Einstein/MyWorkspaceCC1350/empty_CC1350_LAUNCHXL_tirtos_ccs" --include_path="C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/posix/ccs" --include_path="C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/include" --define=DeviceFamily_CC13X0 -g --diag_warning=225 --diag_warning=255 --diag_wrap=off --display_error_number --gen_func_subsections=on --preproc_with_compile --preproc_dependency="CC1350_LAUNCHXL.d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+CC1350_LAUNCHXL_fxns.obj: ../CC1350_LAUNCHXL_fxns.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: ARM Compiler'
+	"C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/bin/armcl" -mv7M3 --code_state=16 --float_support=vfplib -me --include_path="C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source" --include_path="C:/Users/Einstein/MyWorkspaceCC1350/empty_CC1350_LAUNCHXL_tirtos_ccs" --include_path="C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/posix/ccs" --include_path="C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/include" --define=DeviceFamily_CC13X0 -g --diag_warning=225 --diag_warning=255 --diag_wrap=off --display_error_number --gen_func_subsections=on --preproc_with_compile --preproc_dependency="CC1350_LAUNCHXL_fxns.d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+ccfg.obj: ../ccfg.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: ARM Compiler'
+	"C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/bin/armcl" -mv7M3 --code_state=16 --float_support=vfplib -me --include_path="C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source" --include_path="C:/Users/Einstein/MyWorkspaceCC1350/empty_CC1350_LAUNCHXL_tirtos_ccs" --include_path="C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/posix/ccs" --include_path="C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/include" --define=DeviceFamily_CC13X0 -g --diag_warning=225 --diag_warning=255 --diag_wrap=off --display_error_number --gen_func_subsections=on --preproc_with_compile --preproc_dependency="ccfg.d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+empty.obj: ../empty.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: ARM Compiler'
+	"C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/bin/armcl" -mv7M3 --code_state=16 --float_support=vfplib -me --include_path="C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source" --include_path="C:/Users/Einstein/MyWorkspaceCC1350/empty_CC1350_LAUNCHXL_tirtos_ccs" --include_path="C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/posix/ccs" --include_path="C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/include" --define=DeviceFamily_CC13X0 -g --diag_warning=225 --diag_warning=255 --diag_wrap=off --display_error_number --gen_func_subsections=on --preproc_with_compile --preproc_dependency="empty.d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+main_tirtos.obj: ../main_tirtos.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: ARM Compiler'
+	"C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/bin/armcl" -mv7M3 --code_state=16 --float_support=vfplib -me --include_path="C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source" --include_path="C:/Users/Einstein/MyWorkspaceCC1350/empty_CC1350_LAUNCHXL_tirtos_ccs" --include_path="C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/posix/ccs" --include_path="C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.8.LTS/include" --define=DeviceFamily_CC13X0 -g --diag_warning=225 --diag_warning=255 --diag_wrap=off --display_error_number --gen_func_subsections=on --preproc_with_compile --preproc_dependency="main_tirtos.d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+
